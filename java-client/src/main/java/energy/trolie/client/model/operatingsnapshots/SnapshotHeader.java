2fcc2cff2f353781dde828e3a9fb0787a49f2bd4
Aidan Short <aidan.short@gevernova.com>
2026-09-01 11:24:06 -0700

    Remove GE Copyright statements (#20)

--- java-client/src/main/java/energy/trolie/client/model/operatingsnapshots/SnapshotHeader.java
+++ java-client/src/main/java/energy/trolie/client/model/operatingsnapshots/SnapshotHeader.java
@@ -1,18 +1,3 @@
-/*
- *  ===========================================================================
- *
- *  Copyright and Proprietary Information
- *
- *  Copyright (c) 1993, 2024 General Electric Technology GmbH. All Rights Reserved.
- *
- *  NOTE: CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION OF GENERAL ELECTRIC
- *  TECHNOLOGY GMBH AND MAY NOT BE REPRODUCED, TRANSMITTED, STORED, OR COPIED IN
- *  WHOLE OR IN PART, OR USED TO FURNISH INFORMATION TO OTHERS, WITHOUT THE PRIOR
- *  WRITTEN PERMISSION OF GENERAL ELECTRIC TECHNOLOGY GMBH OR GRID SOLUTIONS.
- *
- *  ==========================================================================
- */
-
 package energy.trolie.client.model.operatingsnapshots;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
